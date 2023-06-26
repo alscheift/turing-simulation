7ffb9f2d95eb19d57b698f2b62e04728589bd01d
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-26 16:53:44 +0700

    diagram transition json (wip)

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 import Tape from "./Tape.js";
 
-const a = new Tape();
-a.print();
-a.moveLeft();
-a.moveLeft();
-a.print();
-
-a.moveLeft();
-a.print();
-a.moveRight();
-a.print();
+class Diagram {}
+/*
+const e = {
+    state: {
+        q0: [
+            {
+                before: ["E", "E", "E"],
+                after: ["E", "B", "B"],
+                tapeHeads: ["R", "S", "S"],
+                nextState: "q1",
+            },
+        ],
+    },
+};
+Gambaran diagram transisi dalam (JSON)
+*/
