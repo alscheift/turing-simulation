e203c878372528be76fbc440a5b8c4b774171c3c
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-26 16:25:56 +0700

    Tape class in index.js

---
+++ js/index.js
@@ -0,0 +1,45 @@
+class Tape {
+    constructor() {
+        this.tape = ["B", "B", "C", "B", "B"];
+        this.head = 2;
+    }
+
+    write(value) {
+        this.tape[this.head] = value;
+    }
+
+    read() {
+        return this.tape[this.head];
+    }
+
+    moveLeft() {
+        if (this.head === 0) {
+            this.tape.unshift("B");
+        } else {
+            this.head--;
+        }
+    }
+
+    moveRight() {
+        if (this.head === this.tape.length - 1) {
+            this.tape.push("B");
+        }
+        this.head++;
+    }
+
+    print() {
+        const tempVal = this.read();
+        this.write(`>${tempVal}<`);
+        console.log(this.tape);
+        this.write(tempVal);
+    }
+}
+
+const a = new Tape();
+a.print();
+a.moveLeft();
+a.moveLeft();
+a.print();
+
+a.moveLeft();
+a.print();
