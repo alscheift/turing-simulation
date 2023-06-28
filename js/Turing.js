680eecabf104b6a875f1c97f993135b17d2b22c5
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-29 06:47:57 +0700

    moved turing to Turing.js

--- js/Turing.js
+++ js/Turing.js
@@ -1,5 +1,4 @@
 import Tape from "./Tape.js";
-import Addition from "./Operation/Addition.js";
 
 class Turing {
     Tapes = [];
@@ -70,28 +69,5 @@ class Turing {
         this.currentState = currentTransitionData.nextState;
     }
 }
-console.log("Turing Machine Addition");
-console.log("\nKasus 1 (-2 + 1) = -1 harusnya 0");
-const a = new Turing(Addition, 2, "BB00X1BB".split(""));
-a.runUntilFinish();
-a.printAllTape();
 
-console.log("\nKasus 2 (5 + 4) = 9 harusnya 1111111111");
-const b = new Turing(Addition, 2, "BB11111X1111BB".split(""));
-b.runUntilFinish();
-b.printAllTape();
-
-console.log("\nKasus 3 (4 + -5) = -1 harusnya 0");
-const c = new Turing(Addition, 2, "BB1111X00000BB".split(""));
-c.runUntilFinish();
-c.printAllTape();
-
-console.log("\nKasus 4 (-2 + -3) = -5 harusnya 00000");
-const d = new Turing(Addition, 2, "BB00X000BB".split(""));
-d.runUntilFinish();
-d.printAllTape();
-
-console.log("\nKasus 5 (-4 + 10) = 6 harusnya 111111");
-const e = new Turing(Addition, 2, "BB0000X1111111111BB".split(""));
-e.runUntilFinish();
-e.printAllTape();
+export default Turing;
