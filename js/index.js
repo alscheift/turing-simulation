eb33f0c2f239faf67e491571552ad1f73e32e1d6
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-29 04:32:58 +0700

    Mencoba beberapa kasus addition (valid)

--- js/index.js
+++ js/index.js
@@ -21,6 +21,12 @@ class Turing {
         });
     }
 
+    runUntilFinish() {
+        while (this.isFinished === false) {
+            this.nextMove();
+        }
+    }
+
     nextMove() {
         // Tape Saat ini misal [tape1, tape2] = ['1','B'].
         const currentTape = this.Tapes.map((tape) => tape.read());
@@ -64,10 +70,28 @@ class Turing {
         this.currentState = currentTransitionData.nextState;
     }
 }
-
+console.log("Turing Machine Addition");
+console.log("\nKasus 1 (-2 + 1) = -1 harusnya 0");
 const a = new Turing(Addition, 2, "BB00X1BB".split(""));
-while (a.isFinished === false) {
-    a.nextMove();
-}
-console.log(a.transitionDiagram);
+a.runUntilFinish();
 a.printAllTape();
+
+console.log("\nKasus 2 (5 + 4) = 9 harusnya 1111111111");
+const b = new Turing(Addition, 2, "BB11111X1111BB".split(""));
+b.runUntilFinish();
+b.printAllTape();
+
+console.log("\nKasus 3 (4 + -5) = -1 harusnya 0");
+const c = new Turing(Addition, 2, "BB1111X00000BB".split(""));
+c.runUntilFinish();
+c.printAllTape();
+
+console.log("\nKasus 4 (-2 + -3) = -5 harusnya 00000");
+const d = new Turing(Addition, 2, "BB00X000BB".split(""));
+d.runUntilFinish();
+d.printAllTape();
+
+console.log("\nKasus 5 (-4 + 10) = 6 harusnya 111111");
+const e = new Turing(Addition, 2, "BB0000X1111111111BB".split(""));
+e.runUntilFinish();
+e.printAllTape();
