63636f034103590cf5b8ed11d941e6129348c0ac
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-29 04:13:38 +0700

    Turing NextMove

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ class Turing {
     transitionDiagram = {};
 
     currentState = "q0";
-
+    isFinished = false;
     constructor(operation, jumlahTape, firstTapeInput) {
         this.transitionDiagram = operation.state;
         this.Tapes.push(new Tape(firstTapeInput));
@@ -20,8 +20,52 @@ class Turing {
             tape.print();
         });
     }
+
+    nextMove() {
+        // Tape Saat ini misal [tape1, tape2] = ['1','B'].
+        const currentTape = this.Tapes.map((tape) => tape.read());
+
+        // Transisi saat ini, dari Operasi yang dipilih, misal q0
+        const currentTransition = this.transitionDiagram[this.currentState];
+
+        // Membandingkan value before[...] transisi saat ini dengan tape[...] saat ini
+        const currentTransitionIndex = currentTransition.transitions.findIndex(
+            (transition) => {
+                return transition.before.every((val, index) => {
+                    return val === currentTape[index];
+                });
+            }
+        );
+        // console.log(currentTransitionIndex + " of " + this.currentState);
+        // Kalau transisi yang ditemukan adalah final, maka selesai
+        if (currentTransition.isFinal === true) {
+            this.isFinished = true;
+            return;
+        }
+
+        if (currentTransitionIndex === -1) {
+            // error handling kalau tidak nemu transisi
+            console.log("Error: No transition found");
+        }
+
+        // Mendapatkan perubahan tape dan state selanjutnya
+        const currentTransitionData =
+            currentTransition.transitions[currentTransitionIndex];
+        currentTransitionData.after.forEach((val, index) => {
+            this.Tapes[index].write(val);
+        });
+        currentTransitionData.tapeHeads.forEach((val, index) => {
+            if (val === "R") {
+                this.Tapes[index].moveRight();
+            } else if (val === "L") {
+                this.Tapes[index].moveLeft();
+            }
+        });
+        this.currentState = currentTransitionData.nextState;
+    }
 }
 
 const a = new Turing(Addition, 2, "BB111X11BB".split(""));
-
+a.nextMove();
+console.log(a.transitionDiagram);
 a.printAllTape();
