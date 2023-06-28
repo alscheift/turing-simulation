a621b2d0293c5e1c3adaf62d56c251872549d3e4
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-29 03:45:34 +0700

    Better "addition"

--- js/index.js
+++ js/index.js
@@ -28,35 +28,118 @@ a.moveLeft();
 a.print();
 a.moveRight();
 
-/*
-const e = {
+const Addition = {
     state: {
-        q0: [
-            {
-                before: ["0", "B"],
-                after: ["0", "0"],
-                tapeHeads: ["R", "R"],
-                nextState: "q0",
-            },
-
-            {
-                before: ["X", "B"],
-                after: ["X", "B"],
-                tapeHeads: ["R", "L"],
-                nextState: "q1",
-            },
-
-            isFinal = true,
-        ],
-        q1: [
-            {
-                before: ["E", "E", "E"],
-                after: ["E", "B", "B"],
-                tapeHeads: ["R", "S", "S"],
-                nextState: "q1",
-            },
-        ],
+        q0: {
+            isFinal: false,
+            transitions: [
+                {
+                    before: ["0", "B"],
+                    after: ["0", "0"],
+                    tapeHeads: ["R", "R"],
+                    nextState: "q0",
+                },
+                {
+                    before: ["1", "B"],
+                    after: ["1", "1"],
+                    tapeHeads: ["R", "R"],
+                    nextState: "q0",
+                },
+                {
+                    before: ["X", "B"],
+                    after: ["X", "B"],
+                    tapeHeads: ["R", "L"],
+                    nextState: "q1",
+                },
+            ],
+        },
+        q1: {
+            isFinal: false,
+            transitions: [
+                {
+                    before: ["0", "1"],
+                    after: ["0", "B"],
+                    tapeHeads: ["R", "L"],
+                    nextState: "q1",
+                },
+                {
+                    before: ["1", "0"],
+                    after: ["1", "B"],
+                    tapeHeads: ["R", "L"],
+                    nextState: "q1",
+                },
+                {
+                    before: ["0", "B"],
+                    after: ["0", "0"],
+                    tapeHeads: ["R", "R"],
+                    nextState: "q1",
+                },
+                {
+                    before: ["1", "B"],
+                    after: ["1", "1"],
+                    tapeHeads: ["R", "R"],
+                    nextState: "q1",
+                },
+                {
+                    before: ["0", "0"],
+                    after: ["0", "0"],
+                    tapeHeads: ["S", "R"],
+                    nextState: "q1",
+                },
+                {
+                    before: ["1", "1"],
+                    after: ["1", "1"],
+                    tapeHeads: ["S", "R"],
+                    nextState: "q1",
+                },
+                {
+                    before: ["B", "B"],
+                    after: ["B", "B"],
+                    tapeHeads: ["S", "S"],
+                    nextState: "q2",
+                },
+                {
+                    before: ["B", "0"],
+                    after: ["B", "0"],
+                    tapeHeads: ["R", "S"],
+                    nextState: "q2",
+                },
+                {
+                    before: ["B", "1"],
+                    after: ["B", "1"],
+                    tapeHeads: ["R", "S"],
+                    nextState: "q2",
+                },
+                {
+                    before: ["1", "1"],
+                    after: ["1", "1"],
+                    tapeHeads: ["S", "R"],
+                    nextState: "q2",
+                },
+            ],
+        },
+        q2: {
+            isFinal: true,
+            transitions: [
+                {
+                    before: ["0", "B"],
+                    after: ["0", "0"],
+                    tapeHeads: ["R", "R"],
+                    nextState: "q0",
+                },
+                {
+                    before: ["1", "B"],
+                    after: ["1", "1"],
+                    tapeHeads: ["R", "R"],
+                    nextState: "q0",
+                },
+                {
+                    before: ["X", "B"],
+                    after: ["X", "B"],
+                    tapeHeads: ["R", "L"],
+                    nextState: "q1",
+                },
+            ],
+        },
     },
 };
-Gambaran diagram transisi dalam (JSON)
-*/
