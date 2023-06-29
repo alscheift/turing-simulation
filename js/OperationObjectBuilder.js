f726e1bb20f8c154d4eb73ceda04079b83269b31
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-29 07:31:39 +0700

    OperationObjectBuilder done

--- js/OperationObjectBuilder.js
+++ js/OperationObjectBuilder.js
@@ -1,4 +1,45 @@
 // class to build operation object easier
-class OOB {}
+class OOB {
+    ObjectDiagram = {};
+    constructor(query) {
+        this.ObjectDiagram = {
+            state: {},
+        };
+        query.split(";").forEach((transitionQuery) => {
+            this.addState(transitionQuery);
+        });
+    }
+
+    addState(transitionQuery) {
+        // Extract values inside brackets [q0] and [q1], result is qn
+        const regex = /\[(.*?)\]/g; // magic bruh
+
+        const matches = transitionQuery.match(regex);
+
+        const stateFrom = matches[0].substring(1, matches[0].length - 1);
+        const stateTo = matches[1].substring(1, matches[1].length - 1);
+
+        // Extract values separated by slashes and commas
+        const [before, after, moveTapeto] = transitionQuery
+            .split(/[\/,]/g)
+            .filter((value) => value !== "");
+
+        this.ObjectDiagram.state[stateFrom] = {
+            transitions: [],
+            isFinal: false,
+        };
+
+        this.ObjectDiagram.state[stateFrom]["transitions"].push({
+            before: before.split(""),
+            after: after.split(""),
+            tapeHeads: moveTapeto.split(""),
+            nextState: stateTo,
+        });
+    }
+
+    get() {
+        return this.ObjectDiagram;
+    }
+}
 
 export default OOB;
