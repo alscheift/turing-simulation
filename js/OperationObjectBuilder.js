88b1cda9584b966a79a6e24a33d130c406b97548
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-29 08:10:15 +0700

    addresses lots of bug in operationobject builder
    - second regex
    - object problem

--- js/OperationObjectBuilder.js
+++ js/OperationObjectBuilder.js
@@ -5,9 +5,13 @@ class OOB {
         this.ObjectDiagram = {
             state: {},
         };
-        query.split(";").forEach((transitionQuery) => {
-            this.addState(transitionQuery);
-        });
+
+        query
+            .trim()
+            .split(";")
+            .forEach((transitionQuery) => {
+                this.addState(transitionQuery);
+            });
     }
 
     addState(transitionQuery) {
@@ -18,17 +22,20 @@ class OOB {
 
         const stateFrom = matches[0].substring(1, matches[0].length - 1);
         const stateTo = matches[1].substring(1, matches[1].length - 1);
-
         // Extract values separated by slashes and commas
         const [before, after, moveTapeto] = transitionQuery
-            .split(/[\/,]/g)
+            .replace(/\[.*?\]/g, "")
+            .split(/[\/,]/)
+            .map((value) => value.trim())
             .filter((value) => value !== "");
-
-        this.ObjectDiagram.state[stateFrom] = {
-            transitions: [],
-            isFinal: false,
-        };
-
+        console.log(transitionQuery);
+        if (!this.ObjectDiagram.state[stateFrom]) {
+            this.ObjectDiagram.state[stateFrom] = {
+                transitions: [],
+                isFinal: false,
+            };
+        }
+        // console.log(before);
         this.ObjectDiagram.state[stateFrom]["transitions"].push({
             before: before.split(""),
             after: after.split(""),
