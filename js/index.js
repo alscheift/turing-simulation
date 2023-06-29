b7300bc33a69e59f0c1b5ce0a99a56915bc22b22
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-29 13:06:12 +0700

    Updated index.js

--- js/index.js
+++ js/index.js
@@ -1,28 +1,46 @@
 import Turing from "./Turing.js";
 import Addition from "./Operation/Addition.js";
+import Exponent from "./Operation/Exponent.js";
+/*
+let T = null;
+console.log("Turing Machine Exponent");
+console.log("\nKasus 1 (2^3) = 8 harusnya 11111111");
+T = new Turing(Exponent, 3, "BB110111BB");
+T.runUntilFinish();
+T.printAllTape();
 
-console.log("Turing Machine Addition");
-console.log("\nKasus 1 (-2 + 1) = -1 harusnya 0");
-const a = new Turing(Addition, 2, "BB00X1BB");
-a.runUntilFinish();
-a.printAllTape();
+console.log("\nKasus 2 (0^9) = 0 harusnya BBBB");
+T = new Turing(Exponent, 3, "BB0111111111BB");
+T.runUntilFinish();
+T.printAllTape();
 
-console.log("\nKasus 2 (5 + 4) = 9 harusnya 1111111111");
-const b = new Turing(Addition, 2, "BB11111X1111BB");
-b.runUntilFinish();
-b.printAllTape();
+console.log("\nKasus 3 (12^0) = 1 harusnya 1");
+T = new Turing(Exponent, 3, "BB1111111111110BB");
+T.runUntilFinish();
+T.printAllTape();
 
-console.log("\nKasus 3 (4 + -5) = -1 harusnya 0");
-const c = new Turing(Addition, 2, "BB1111X00000BB");
-c.runUntilFinish();
-c.printAllTape();
+console.log("\nKasus 4 (1^9) = 1 harusnya 1");
+T = new Turing(Exponent, 3, "BB10111111111BB");
+T.runUntilFinish();
+T.printAllTape();
 
-console.log("\nKasus 4 (-2 + -3) = -5 harusnya 00000");
-const d = new Turing(Addition, 2, "BB00X000BB");
-d.runUntilFinish();
-d.printAllTape();
+console.log("\nKasus 5 (4^2) = 16 harusnya 1111111111111111");
+T = new Turing(Exponent, 3, "BB1111011BB");
+T.runUntilFinish();
+T.printAllTape();
 
-console.log("\nKasus 5 (-4 + 10) = 6 harusnya 111111");
-const e = new Turing(Addition, 2, "BB0000X1111111111BB");
-e.runUntilFinish();
-e.printAllTape();
+console.log("\nKasus 6 (0^5) = 0 harusnya ");
+T = new Turing(Exponent, 3, "BB011111BB");
+T.runUntilFinish();
+T.printAllTape();
+
+console.log("\nKasus 7 (0^0) = 1 harusnya 1");
+T = new Turing(Exponent, 3, "BB0BB");
+T.runUntilFinish();
+T.printAllTape();
+
+console.log("\nKasus 8 (3^2) = 9 harusnya 111111111");
+T = new Turing(Exponent, 3, "BB111011BB");
+T.runUntilFinish();
+T.printAllTape();
+*/
