9b530889286f3fcbb5045d73c61da35ead73e28b
Alfiki Diastama Afan Firdaus <alfiki@student.uns.ac.id>
2023-07-01 22:16:19 +0700

    Tidy up inner page

--- js/cellStyle.js
+++ js/cellStyle.js
@@ -1,9 +1,9 @@
 const cellStyle = {
-    head: "border-blue-500 bg-red-500 hover:bg-blue-500 text-white",
-    standard: "border-blue-500 bg-blue-600 hover:bg-red-500 text-white",
+    head: "bg-purple-500 hover:bg-indigo-500 text-white",
+    standard: "bg-indigo-600 hover:bg-indigo-500 text-white",
 
     defaultAll:
-        "border-2 px-6 py-2 font-mono font-medium tracking-wide capitalize transition-colors duration-300 transform rounded-lg",
+        "px-6 py-2 font-mono font-medium tracking-wide capitalize transition-colors duration-300 transform rounded-lg",
 
     getStyle(isHead) {
         const def = this.defaultAll + " ";
